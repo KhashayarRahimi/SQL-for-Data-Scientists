104541db7c8e565e0b93ef6ccae207feceb033af
Khashayar Rahimi <134632638+KhashayarRahimi@users.noreply.github.com>
2023-10-15 12:15:43 -0700

    Add files via upload

---
+++ Chapter 2.sql
@@ -0,0 +1,12 @@
+SELECT * FROM farmers_market.customer
+
+SELECT 
+    *
+FROM
+    farmers_market.customer
+ORDER BY customer_last_name , customer_first_name
+LIMIT 10
+
+SELECT customer_first_name, customer_id FROM farmers_market.customer
+ORDER BY customer_first_name
+
